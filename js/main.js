b1a526600152de590e97bb983b82ace8059d0f8f
tarek <tarekt820@gmail.com>
2018-12-10 00:28:17 +0200

    add some styling to the form in mobiles version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
-
 document.querySelector('.login-pop').addEventListener('click' , function () {
     document.querySelector('.login-pop-up').style.display = 'flex' ;
+    document.querySelector('.navbar-collapse').style.display = 'none'
 })
 
 document.querySelector('.btn-close').addEventListener('click' , function () {
