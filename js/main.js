d02b10e0ea66cab58c1d87cf0ca02d280383866c
tarek <tarekt820@gmail.com>
2018-12-10 00:16:16 +0200

    add js/

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+document.querySelector('.login-pop').addEventListener('click' , function () {
+    document.querySelector('.login-pop-up').style.display = 'flex' ;
+})
+
+document.querySelector('.btn-close').addEventListener('click' , function () {
+    document.querySelector('.login-pop-up').style.display = 'none' ;
+})
